a97136ca641ff4d1addc33e5c6a94f5402f8545e
ObSp <150492054+ObSp@users.noreply.github.com>
2024-01-05 10:56:05 -0500

    Update test.lua

--- luaClass/test.lua
+++ luaClass/test.lua
@@ -1,4 +1,4 @@
-local class = require("luaClasses")
+local class = require("luaClass/luaClasses")
 
 local food = class({
     __init__ = function(self, args)
@@ -13,5 +13,20 @@ local food = class({
     FoodType = "",
 })
 
-local Carrot = food({FoodType = "Carrot"})
-Carrot:PrintType()
+local animal = class({
+    __init__ = function(self, args)
+        self.AnimalType = args.AnimalType
+        self.Damage = args.Damage
+        self.Noise = args.Noise
+        return self
+    end,
+    
+    MakeNoise = function(self)
+        print(self.Noise)
+    end
+})
+
+local beef = food({FoodType = "Beef"})
+local cow = animal({AnimalType = "Cow", Damage = 1, Noise = "MOOOOOOO"})
+beef:PrintType()
+cow:MakeNoise()
